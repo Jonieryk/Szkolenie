83d622120823b5b8ada106a3749e1c1dc41a0842
jonatan.joni1@gmail.com <jonatan.joni1@gmail.com>
2024-09-25 12:21:58 +0200

    Modified the script to backup the docker mysql database and the wp_data wordpress volume instead. The data of the host services is no longer backed up.

--- wordpress_backup.sh
+++ wordpress_backup.sh
@@ -9,10 +9,10 @@ AMOUNT=8
 password=$(cat $HOME_DIR/.backup_password)
 
 #SQL wordpress adtabase backup
-mysqldump -u backup -p$password --databases wordpress > $HOME_DIR/backups/wrdsql-`date "+%Y-%m-%d-%H-%M"`.sql
+docker exec wordpress-docker-compose-db-1 mysqldump -u backup -p$password --databases wordpress > $HOME_DIR/backups/wrdsql-`date "+%Y-%m-%d-%H-%M"`.sql
 
 #Wordpress data backup
-echo "$password" | sudo -S tar -zcvf $HOME_DIR/backups/wrdusr-`date "+%Y-%m-%d-%H-%M"`.tar.gz /usr/share/wordpress
+echo "$password" | sudo -S tar -zcvf $HOME_DIR/backups/wrdusr-`date "+%Y-%m-%d-%H-%M"`.tar.gz $HOME_DIR/Wordpress-docker-compose/wp_data
 
 # Check if there are more than the specified amount of SQL backups and delete the old ones
 find $HOME_DIR/backups -name 'wrdsq*' -type f | sort | head -n -$AMOUNT | xargs -r rm
